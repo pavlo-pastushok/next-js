a76954ab9e51185aa840a5b3fc6c37d793437b1d
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2024-11-04 12:07:58 +0200

    -

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require('path')
 const nextConfig = {
     images: { unoptimized: true },
-    output: 'export',
     pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
     reactStrictMode: true,
     sassOptions: {
